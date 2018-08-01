08f2c9af00d793c4d276ab2d24a976b13066d3be
Rameez <rameez.t@live.com>
2018-08-01 14:16:19 +0100

    Initial commit

---
+++ pizza_shop/models/pizza_order.rb
@@ -0,0 +1,84 @@
+require('pg')
+require_relative('../db/sql_runner.rb')
+
+class PizzaOrder
+
+  attr_accessor :topping, :quantity
+  attr_reader :id, :customer_id
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @topping = options['topping']
+    @quantity = options['quantity'].to_i()
+    @customer_id = options['customer_id'].to_i()
+  end
+
+  def save()
+    sql = "INSERT INTO pizza_orders
+    (
+      topping,
+      quantity,
+      customer_id
+    ) VALUES
+    (
+      $1, $2, $3
+    )
+    RETURNING id"
+    values = [@topping, @quantity, @customer_id]
+    results = SqlRunner.run(sql, values)
+    @id = results[0]["id"].to_i
+  end
+
+  def update()
+    db = PG.connect({ dbname: 'pizza_shop', host: 'localhost' })
+    sql = "
+    UPDATE pizza_orders SET (
+      topping,
+      quantity
+    ) =
+    (
+      $1,$2,$3
+    )
+    WHERE id = $4"
+    values = [@topping, @quantity, @customer_id, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM pizza_orders where id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM pizza_orders WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run(sql, values)
+    order_hash = results.first
+    order = PizzaOrder.new(order_hash)
+    return order
+  end
+
+  def self.delete_all()
+
+    sql = "DELETE FROM pizza_orders"
+    SqlRunner.run(sql)
+
+  end
+
+  def self.all()
+    sql = "SELECT * FROM pizza_orders"
+    orders = SqlRunner.run(sql)
+    return orders.map { |order| PizzaOrder.new(order) }
+  end
+
+  def customer()
+
+    sql = "SELECT * FROM customers WHERE id = $1"
+    values = [@customer_id]
+    results = SqlRunner.run(sql, values)
+    return Customer.new(results[0])
+
+  end
+
+end
